985f1fceab2230da679776572735a6dd2d26fe34
rijviislam <rijviislam54@gmail.com>
2024-11-23 18:16:55 +0600

    feat: create hero section

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        oswald: ['Oswald'],
+        manrope: ['Manrope'],
+      
+      },
+    },
+  },
+  plugins: [],
+}
