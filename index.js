4e2d3422a33bf9f9b1ad12b488a0d1e0f6a416e7
rijviislam <rijviislam54@gmail.com>
2024-11-24 03:30:33 +0600

    revert: reverts a previos commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+
 const mySwiperButton = new Swiper(".mySwiper", {
     loop: true,
-    navigation: false, // Disable default navigation buttons
   });
   
-  // Add event listeners for custom buttons
   document.querySelector(".custom-prev").addEventListener("click", () => {
     mySwiperButton.slidePrev();
   });
