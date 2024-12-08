d1a5c536dfcd0166062d0f94a0ebde0b1b765414
rijviislam <rijviislam54@gmail.com>
2024-12-09 05:31:14 +0600

    revert: reverts a previous commit

---
+++ modal.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const firstModal = document.getElementById("my_modal_3");
+    const secondModal = document.getElementById("my_modal_2");
+    const thirdModal = document.getElementById("my_modal_4");
+    const loginLink = document.getElementById("loginAcc");
+    const createLink = document.getElementById("createAcc");
+    const forgetLink = document.getElementById("forgetPass")
+    const back = document.getElementById("back");
+  
+    if (firstModal && secondModal && loginLink && createLink && forgetLink) {
+      loginLink.addEventListener("click", () => {
+        firstModal.close();
+        secondModal.showModal(); 
+      });
+  
+      createLink.addEventListener("click", () => {
+        secondModal.close(); 
+        firstModal.showModal();
+      });
+      forgetLink.addEventListener("click", ()=> {
+        firstModal.close();
+        secondModal.close();
+        thirdModal.showModal();
+      })
+      back.addEventListener("click", () => {
+        firstModal.close();
+        thirdModal.close();
+        secondModal.showModal();
+      })
+    } else {
+      console.error(
+        "Modal not found"
+      );
+    }
+  });
+
+  console.log("uploadPrescription")
