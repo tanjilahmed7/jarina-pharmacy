67d2f0c16c1990da5bec23f5e8ce4d811b5c1f6c
rijviislam <rijviislam54@gmail.com>
2024-12-17 01:16:51 +0600

    feat: added Inventory page

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ tabButton.forEach((tab, idx) => {
 
 
 
-function toggleFunc() {
+function toggleFuncMenu() {
   const nav = document.querySelector(".myNav");
   const openButton = document.getElementById("openMenuButton");
   const closeButton = document.getElementById("closeMenuButton");
@@ -150,6 +150,7 @@ function showModal() {
 
 // Function to close modal
 function toggleFunc() {
+ 
   const modal = document.getElementById('my_modal_3');
   if (modal) {
     modal.close();
