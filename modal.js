52290f3059164864dcb22f4310fdaee788819f23
rijviislam <rijviislam54@gmail.com>
2024-12-19 23:32:20 +0600

    feat: added billing adsress page

--- modal.js
+++ modal.js
@@ -35,14 +35,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  function showModal() {
+function showModal() {
   const modal = document.getElementById('my_modal_3');
   if (modal) {
     modal.showModal();
   }
 }
+
 function toggleFunc() {
- 
+  const modal = document.getElementById('my_modal_3');
+  if (modal) {
+    modal.close();
+  }
+}
+function toggleFunc() {
   const modal = document.getElementById('my_modal_3');
   if (modal) {
     modal.close();
