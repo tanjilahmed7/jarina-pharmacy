e9923f11b0818b3374c07b09ac724a235e0f8aee
rijviislam <rijviislam54@gmail.com>
2024-12-29 05:37:55 +0600

    revert : reverts a previous commit

--- modal.js
+++ modal.js
@@ -69,3 +69,12 @@ function showModalDashboard() {
     modal.showModal();
   }
 }
+
+function showModalPrescription2() {
+  const modal = document.getElementById('my_modal_prescription2');
+  const modal2 = document.getElementById('my_modal_prescription');
+  if (modal) {
+    modal2.close();
+    modal.showModal();
+  } 
+}
