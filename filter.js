d5ea0c19eac0554c6cfd7f16202f7867c9fe657d
rijviislam <rijviislam54@gmail.com>
2024-12-28 06:19:54 +0600

    feta: added some page on admin panel

--- filter.js
+++ filter.js
@@ -14,3 +14,20 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const filterButton = document.getElementById('notiButton');
+    const searchBox = document.getElementById('notiBox');
+  
+    filterButton.addEventListener('click', (event) => {
+      event.stopPropagation(); 
+      searchBox.classList.toggle('hidden'); 
+    });
+  
+    document.addEventListener('click', (event) => {
+      if (!searchBox.contains(event.target) && event.target !== filterButton) {
+        searchBox.classList.add('hidden'); 
+      }
+    });
+  });
