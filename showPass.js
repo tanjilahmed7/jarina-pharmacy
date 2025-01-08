4dc76775325f7415480766977c658be5093a3303
rijviislam <rijviislam54@gmail.com>
2025-01-09 00:42:01 +0600

    fix: modal has been fixed

--- showPass.js
+++ showPass.js
@@ -1,24 +1,39 @@
-function togglePasswordVisibility() {
-    const passwordInput = document.getElementById('passwordInput');
-    const eyeIcon = document.getElementById('eyeIcon');
+// function togglePasswordVisibility() {
+//     const passwordInput = document.getElementById('passwordInput');
+//     const eyeIcon = document.getElementById('eyeIcon');
   
+//     if (passwordInput.type === 'password') {
+//       passwordInput.type = 'text'; 
+//       eyeIcon.src = './assets/passeye.svg'; 
+//     } else {
+//       passwordInput.type = 'password'; 
+//       eyeIcon.src = './assets/passeye.svg'; 
+//     }
+//   }
+  
+//   function togglePasswordVisibility2() {
+//     const passwordInput = document.getElementById('passwordInput2');
+//     const eyeIcon = document.getElementById('eyeIconConfirm');
+  
+//     if (passwordInput.type === 'password') {
+//       passwordInput.type = 'text'; 
+//       eyeIcon.src = './assets/passeye.svg'; 
+//     } else {
+//       passwordInput.type = 'password'; 
+//       eyeIcon.src = './assets/passeye.svg'; 
+//     }
+//   }
+
+
+
+  function togglePasswordVisibility(eyeIcon) {
+    const passwordInput = eyeIcon.previousElementSibling; 
     if (passwordInput.type === 'password') {
-      passwordInput.type = 'text'; 
+      passwordInput.type = 'text';
       eyeIcon.src = './assets/passeye.svg'; 
     } else {
-      passwordInput.type = 'password'; 
+      passwordInput.type = 'password';
       eyeIcon.src = './assets/passeye.svg'; 
     }
   }
-  function togglePasswordVisibility2() {
-    const passwordInput = document.getElementById('passwordInput2');
-    const eyeIcon = document.getElementById('eyeIconConfirm');
   
-    if (passwordInput.type === 'password') {
-      passwordInput.type = 'text'; 
-      eyeIcon.src = './assets/passeye.svg'; 
-    } else {
-      passwordInput.type = 'password'; 
-      eyeIcon.src = './assets/passeye.svg'; 
-    }
-  }
